bdb515fa21e0fe51c126016a187eafbac0039ddd
Zhambyl <ogremagizk4@gmail.com>
2022-07-01 17:08:29 +0600

    firstcommit

---
+++ src/Components/employees-list-item/employees-list-item.js
@@ -0,0 +1,31 @@
+import './employees-list-item.css'
+
+const Employeeslistitem = ({name, salary, increase}) => {
+
+    let listGroupItem = 'list-group-item d-flex justify-content-between';
+    if (increase) {
+       listGroupItem = listGroupItem + ' increase'
+    }
+
+    return (
+        <li className={listGroupItem}>
+            <span className='list-group-item-label'> {name} </span>
+            <input type='text' className='list-group-item-input' defaultValue={salary + '$'} />
+            <div className='d-flex justify-content-center align-items-center'>
+                <button
+                    type='button'
+                    className='btn-cookie btn-sm '>
+                    <i className='fas fa-cookie'></i>
+                </button>
+                <button
+                    type='button'
+                    className='btn-trash btn-sm '>
+                    <i className='fas fa-trash'></i>
+                </button>
+                <i className='fas fa-star'></i>
+            </div>
+        </li>
+    )
+}
+
+export default Employeeslistitem;
---
+++ src/Components/employees-list/employees-list.js
@@ -0,0 +1,20 @@
+import Employeeslistitem from "../employees-list-item/employees-list-item"
+import './employees-list.css'
+
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(elem => {
+        const {id, ...elemProps} = elem 
+        return (
+            <Employeeslistitem key={id} {...elemProps}/>
+        )
+    });
+
+    return (
+        <ul className='app-list list-group'>
+           {elements}
+        </ul>
+    )
+}
+
+export default EmployeesList;
